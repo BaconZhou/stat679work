95a4702a636af563785ce9594579f4eeaf82649f
Peigen Zhou <pzhou9@wisc.edu>
2016-10-07 11:00:21 -0500

    updata use ere

--- hw1/scripts/summarizeSNaQres.sh
+++ hw1/scripts/summarizeSNaQres.sh
@@ -1,14 +1,11 @@
-echo analysis, h, CPUtime > ../results/summary.csv
+echo analysis,h,CPUtime > ../results/summary.csv
 
 for file in ../data/log/*.log
 do
-    analysis=`grep rootname $file | cut -f 2 -d ':'`
+    analysis=`grep rootname ${file} | grep -o [:/].* | grep -E "[[:alnum:]]\w+" -o`
     outname="${file%.*}.out"
     outname="../data/out/${outname##*/}"
-    h=`grep hmax $file | head -n 1 | cut -f 2 -d '=' | cut -f1 -d,`
-    CPUtime=`grep Elapsed\ time "${outname}"`
-    CPUtime=${CPUtime##*:}
-    CPUtime=${CPUtime%% seconds*}
-
-    echo $analysis, $h, $CPUtime >> ../results/summary.csv
+    h=`grep hmax ${file} | head -n 1|grep -E "\d" -o`
+    CPUtime=`grep -E "Elapsed time. \d+\.\d+" -o "${outname}"| grep -E "\d+\.\d+" -o`
+    echo $analysis,$h,$CPUtime>> ../results/summary.csv
 done
